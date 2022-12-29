c903bc48a7d0ff2bf7b383782f884fd552906c1c
unknown <mdalihaidar28@gmail.com>
2022-12-29 20:36:17 +0600

    First commit

---
+++ Form7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp13
+{
+    public partial class Form7 : Form
+    {
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            
+    }
+    }
+}
